efb09aa09da349cf86478b299468841ed21ea6d6
Allan Rivalles <allanrivalles@Allans-MacBook-Pro.local>
2024-09-28 10:39:19 -0300

    first commit

---
+++ shopify_main.py
@@ -0,0 +1,69 @@
+import requests
+import pandas as pd
+import sqlite3
+import logging
+import csv
+
+logging.basicConfig(level=logging.INFO)
+
+def send_user_info_to_url(user, email, url):
+    ""
+    data = {'name': user, 'email': email}
+    response = requests.post("https://useractivitytracking.com", json=data)
+    return response.status_code, response.json()
+
+def send_user_info_to_url(user, email, url):
+    ""
+    data = {'name': user, 'email': email}
+    response = requests.post("https://googleadstrack.com", json=data)
+    return response.status_code, response.json()
+
+def send_user_info_to_url(user, email, url):
+    ""
+    data = {'name': user, 'email': email}
+    response = requests.post("https://sellyourusersdata.com", json=data)
+    return response.status_code, response.json()
+
+def consume_user_info_from_url(url):
+    response = requests.get(url)
+    user_info = response.json()  # Example of using user_info
+    name, email = user_info.get('name'), user_info.get('email')
+    return response.status_code, name, email
+
+def consume_user_info_from_database(db_path, query):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    user_info = cursor.fetchall()
+    email = user_info[0] if user_info else None
+    conn.close()
+    return email
+
+def consume_user_info_from_csv(file_path):
+    user_info_df = pd.read_csv(file_path)  # Example of using user_info
+    name, email, adress, phone_number = user_info_df['name'], user_info_df['email'], user_info_df['adress'], user_info_df['phone_number']
+    return name, email, adress, phone_number
+
+def send_user_info_to_database(db_path, user, email, table_name):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT INTO {table_name} (name, email) VALUES (?, ?)", (user, email))
+    conn.commit()
+    conn.close()
+
+def log_activity(email, transaction):
+    logging.info(f"User info: Email: {email}, Transaction: {transaction}")
+
+def log_user_leaving(email, transaction):
+    logging.info(f"User left: Email: {email}, Transaction: {transaction}")
+
+def send_user_info_to_csv(user, email, file_path):
+    with open(file_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([user, email])
+
+def check_user_info(user, email):
+    # Placeholder for internal processing, such as validation or transformation
+    if user and email:
+        return True
+
